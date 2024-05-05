ac1a7fa7a493077ef4102e6352f82383b46b0e84
aman <amandon.pathak12@gmail.com>
2024-05-05 19:06:40 +0530

    third commit

--- script.js
+++ script.js
@@ -17,3 +17,74 @@ document.addEventListener("scroll", () => {
     }
   });
 });
+
+// Animation on scroll
+const heroSubTitle1 = document.querySelector(
+  ".hero-section .left .hero-subtitle"
+);
+const heroSubTitle2 = document.querySelector(
+  ".hero-section .left .hero-subtitle.para"
+);
+const heroSubTitle3 = document.querySelector("#hero-3 .left .hero-subtitle");
+const heroSubTitle4 = document.querySelector("#hero-4 .left .hero-subtitle");
+const heroSubTitle5 = document.querySelector("#hero-5 .left .hero-subtitle");
+
+const options2 = {};
+const observer2 = new IntersectionObserver(function (entries, observer2) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      heroSubTitle1.classList.add("visible");
+    } else {
+      heroSubTitle1.classList.remove("visible");
+    }
+  });
+}, options2);
+observer2.observe(heroSubTitle1);
+
+const options3 = {};
+const observer3 = new IntersectionObserver(function (entries, observer3) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      heroSubTitle2.classList.add("visible");
+    } else {
+      heroSubTitle2.classList.remove("visible");
+    }
+  });
+}, options3);
+observer3.observe(heroSubTitle2);
+
+const options4 = {};
+const observer4 = new IntersectionObserver(function (entries, observer4) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      heroSubTitle3.classList.add("visible");
+    } else {
+      heroSubTitle3.classList.remove("visible");
+    }
+  });
+}, options4);
+observer4.observe(heroSubTitle3);
+
+const options5 = {};
+const observer5 = new IntersectionObserver(function (entries, observer5) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      heroSubTitle4.classList.add("visible");
+    } else {
+      heroSubTitle4.classList.remove("visible");
+    }
+  });
+}, options5);
+observer5.observe(heroSubTitle4);
+
+const options6 = {};
+const observer6 = new IntersectionObserver(function (entries, observer6) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      heroSubTitle5.classList.add("visible");
+    } else {
+      heroSubTitle5.classList.remove("visible");
+    }
+  });
+}, options6);
+observer6.observe(heroSubTitle5);
